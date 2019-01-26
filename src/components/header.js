da494301a37ec4d0c496a10b608314c691cf9e4e
rohitverma1111 <rohit.verma1111@gmail.com>
2019-01-26 21:15:19 +0530

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+
+class Header extends Component {
+
+    state = {
+        keywords: ''
+    }
+
+    inputChange = (event) => {
+        this.setState({
+            keywords: event.target.value
+        });
+    }
+
+    render(){
+        return(
+            <header className={this.state.active }>
+                <div className="logo">logo</div>
+                <input onChange={this.inputChange} type="text"/>
+            </header>
+        )
+    }
+} 
+export default Header;
---
+++ src/components/news_list_item.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {css} from 'glamor';
+
+const NewsItem = ({item}) => {
+    let news_item = css({
+        padding: '20px',
+        boxSizing: 'border-box',
+        borderBottom: '1px solid #ccc',
+        ':hover':{
+            color: 'red'
+        },
+        '@media(max-width: 500px)':{
+            color: 'blue'
+        }
+    })
+    let item_greay = css({
+        background : "lightgrey"
+    })
+    return(
+        <div className={`${news_item} ${item_greay}`}>
+                <h3>{item.title}</h3>
+                <p>{item.feed}</p>
+            </div>
+    )
+}
+
+export default NewsItem;
