ba4ef24c0dd2a1397458ff3b9ac99a72a7291a82
rohitverma1111 <rohit.verma1111@gmail.com>
2019-09-25 23:13:49 +0530

    update

---
+++ router_lifecycles/src/components/profiles.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Profiles = () =>{
+    return(
+        <div>Profile</div>
+    )
+}
+export default Profiles;
--- router_lifecycles/src/index.js
+++ router_lifecycles/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profiles';
 
 const App = () =>{
     return(
-        <div>Home</div>
+        <BrowserRouter>
+            <div>
+                <Route path="/" exact  component={Home} />
+                <Route path="/posts" component={Posts} />
+                <Route path="/profile" component={Profile} />
+            </div>
+        </BrowserRouter>
     )
 }
 ReactDom.render(
